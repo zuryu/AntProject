e672ed98c0d8f08dbd2c8a516fb3d7030866f02f
Mary Gejda <solryu0@gmail.com>
2015-03-30 00:07:20 +0100

    Working on user interface class.

--- AntProject/src/View.java
+++ AntProject/src/View.java
@@ -1,5 +1,8 @@
 
+import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
+import javax.swing.JApplet;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -18,13 +21,28 @@ public class View {
     }
     
     public View(){
-    }
-    
-    private void showIntro(){
         
     }
     
-    public void hexagon(Graphics2D g2, int x, int y){
-        g2.draw(new Line2D.Double(x, y + 0.25, x + 0.5, y));
+    public void drawGUI(){
+        frame = new JFrame();
     }
+    
+    public class Grid extends JPanel {
+        
+    }
+//    
+//    private void showIntro(){
+//        
+//    }
+//    
+//    public void hexagon(Graphics2D g2, int x, int y){
+//        g2.draw(new Line2D.Double(x, y + 0.25, x + 0.5, y));
+//    }
+//    
+//    @Override
+//    public void paint(Graphics g){
+//        Graphics2D g2 = (Graphics2D) g;
+//        hexagon(g2, 1, 1);
+//    }
 }
